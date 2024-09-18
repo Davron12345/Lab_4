95250a6bacee7dd13d777914227a45e705e698da
d.abidov <d.abidov@newuu.uz>
2024-09-18 12:32:38 +0500

    git init

---
+++ Lab_4.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+//    Problem1
+//   int x;
+//   cout<<"Enter x = ";
+//   cin>>x;
+//   if (x>=0)
+//       cout<<"Positive"<<endl;
+//    else
+//        cout<<"Negetive";
+    //Problem2
+//    int x;
+//    cout<<"Enter x = ";
+//    cin>>x;
+//    if (x>=0) {
+//        if ((x % 2) == 0) {
+//            cout << "The number is positive and even." << endl;
+//        } else {
+//        cout << "The number is positive and odd." << endl;
+//        }
+//    }else{
+//        if ((x % 2) == 0) {
+//            cout << "The number is negative and even." << endl;
+//        }
+//        else{
+//            cout << "The number is negative and odd." << endl;
+//            }
+//    }
+    //Problem3
+//    float a;
+//    float b;
+//    float c;
+//    cout<<"Enter a = ";
+//    cin>>a;
+//    cout<<"Enter b = ";
+//    cin>>b;
+//    cout<<"Enter c = ";
+//    cin>>c;
+//    if ((a>b)&&(a>c))
+//        cout<<a<<" is the largest"<<endl;
+//    else if ((b>a)&&(b>c))
+//        cout<<b<<" is the largest"<<endl;
+//    else if ((c>a)&&(c>b))
+//        cout<<c<<" is the largest"<<endl;
+    //Problem4
+//    int a, b;
+//    cout<<"Enter a = ";
+//    cin>>a;
+//    cout<<"Enter b = ";
+//    cin>>b;
+//    if ((a%b)==0)
+//        cout<<a<<" is divisible by "<<b<<endl;
+//    else
+//        cout<<a<<" is not divisible by "<<b<<endl;
+    //Problem5
+//    int a;
+//    cout<<"Enter a year = ";
+//    cin>>a;
+//    if ((a%4)==0)
+//        if (((a%100)==0)&&((a%400)!=0))
+//            cout<<a<<" is not a leap year."<<endl;
+//        else
+//            cout<<a<<" is a leap year."<<endl;
+//    else
+//        cout<<a<<" is not a leap year."<<endl;
+    //Problem6
+//    float a, b, c, dis ;
+//    cout<<"Enter a = ";
+//    cin>>a;
+//    cout<<"Enter b = ";
+//    cin>>b;
+//    cout<<"Enter c = ";
+//    cin>>c;
+//    dis=b*b-4*a*c;
+//    if (dis>=0){
+//        cout<<"x1 = "<<((-1*b)+sqrt(dis))/(2.0*a)<<endl;
+//        cout<<"x2 = "<<((-1*b)-sqrt(dis))/(2.0*a)<<endl;
+//    }
+//    else
+//        cout<<"Undifened";
+    //Problem 7
+//    float a;
+//    float b;
+//    float c;
+//    cout<<"Enter angle a = ";
+//    cin>>a;
+//    cout<<"Enter angle b = ";
+//    cin>>b;
+//    cout<<"Enter angle c = ";
+//    cin>>c;
+//    if((a+b+c)==180)
+//        cout<<"The triangle is valid"<<endl;
+//    else
+//        cout<<"The triangle is not valid";
+    //Problem8
+//    char a;
+//    cout<<"Enter a character = ";
+//    cin>>a;
+//    if ((65<=(int)a)&&((int)a<=90))
+//        cout<<"Uppercase alphabet";
+//    else if ((97<=(int)a)&&((int)a<=122))
+//        cout<<"Lower- case alphabet";
+//    else
+//        cout<<"It is not an alphabet";
+    //Problem9
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
